02b8691ce48350f0b993f75a01a51a0f78c09dbc
Sam Doran <sdoran@redhat.com>
2024-03-14 14:54:16 -0400

    Do the work of creating the relationships in the dataclass

--- koku/api/settings/tags/mapping/query_handler.py
+++ koku/api/settings/tags/mapping/query_handler.py
@@ -5,6 +5,8 @@
 """Query handler for Tag Mappings."""
 import dataclasses
 import uuid
+
+
 @dataclasses.dataclass(frozen=True)
 class TagKey:
     uuid: str
@@ -12,42 +14,20 @@ class TagKey:
     source_type: str
 
 
-@dataclasses.dataclass
+@dataclasses.dataclass(frozen=True)
 class Relationship:
     parent: TagKey
-    child: TagKey = None
     children: list[TagKey] = dataclasses.field(default_factory=list)
 
-    def __post_init__(self):
-        if self.child is None:
-            self.__dataclass_fields__ = {key: value for key, value in self.__dataclass_fields__.items() if key != "child"}
-
     @classmethod
-    def create_from_data(cls, data: list[dict[str: dict[str: uuid.UUID | str]]]):
-        return [
-            cls(
-                parent=TagKey(**item["parent"]),
-                child=TagKey(**item["child"]),
-            )
-            for item in data
-        ]
+    def create_list_of_relationships(cls, data: list[dict[str : dict[str : uuid.UUID | str]]]) -> list["Relationship"]:
+        result = {}
+        for item in data:
+            parent = TagKey(**item["parent"])
+            child = TagKey(**item["child"])
+            if parent in result:
+                result[parent].children.append(child)
+            else:
+                result[parent] = Relationship(parent, [child])
 
-    @property
-    def all_children(self):
-        return [self.child, *self.children]
-
-
-def format_tag_mapping_relationship(relationships: list[Relationship]) -> list[dict[str: uuid.UUID | str]]:
-    formatted_relationships = {}
-    for relationship in relationships:
-        parent_uuid = relationship.parent.uuid
-        if parent_uuid not in formatted_relationships:
-            formatted_relationships[parent_uuid] = Relationship(
-                parent=relationship.parent,
-                # child=relationship.child,
-                children=relationship.all_children,  # ?
-            )
-        else:
-            formatted_relationships[parent_uuid].children.extend(relationship.all_children)
-
-    return [dataclasses.asdict(relationship) for relationship in formatted_relationships.values()]
+        return list(result.values())
