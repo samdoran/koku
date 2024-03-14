3191f8ea6dcaed25c9a95f3d7f97667e779a41e1
Sam Doran <sdoran@redhat.com>
2024-03-14 12:16:07 -0400

    Add logging and a check to prevent client from initializing multiple times

--- koku/api/apps.py
+++ koku/api/apps.py
@@ -3,6 +3,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 """API application configuration module."""
+import logging
 import os
 
 from django.apps import AppConfig
@@ -10,11 +11,18 @@ from django.apps import AppConfig
 from koku.feature_flags import UNLEASH_CLIENT
 
 
+LOG = logging.getLogger(__name__)
+
+
 class ApiConfig(AppConfig):
     """API application configuration."""
 
     name = "api"
+    _unleash_initialized = False
 
     def ready(self):
-        UNLEASH_CLIENT.unleash_instance_id += f"-{os.getpid()}"
-        UNLEASH_CLIENT.initialize_client()
+        if not ApiConfig._unleash_initialized:
+            UNLEASH_CLIENT.unleash_instance_id += f"-{os.getpid()}"
+            LOG.info(f"{LOG.name} Iniliatizing unleash client {UNLEASH_CLIENT.unleash_instance_id}")
+            UNLEASH_CLIENT.initialize_client()
+            ApiConfig._unleash_initialized = True
